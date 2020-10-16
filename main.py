1c36aa23ae99bac22412740316aecba293594a5e
Juliana Figueiredo de Andrade <julianafandrade@hotmail.com>
2020-10-15 22:37:48 -0300

    feat: CORS adicionado

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from typing import Optional
 from fastapi import FastAPI
 from pymongo import MongoClient
+from fastapi.middleware.cors import CORSMiddleware
 
 import get_elements as ge
 import svm as s
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -24,3 +32,17 @@ def requisicao(id: str):
     for i in y_pred:  # Número de Colunas
         objeto["data"].append(int(i))
     return objeto
+
+
+
+# @app.get("/get-rna/{id}")
+# def requisicao(id: str):
+#     X_test = ge.getValues(id)
+#     y_pred = s.svm(X_test["data"])
+#     objeto = {
+#         "personID": id,
+#         "data": []
+#     }
+#     for i in y_pred:  # Número de Colunas
+#         objeto["data"].append(int(i))
+#     return objeto
